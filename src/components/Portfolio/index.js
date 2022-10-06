d0f084f2a1a08179bb1755cea5c6efb37ace7d18
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-10-06 09:01:48 -0500

    added new project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,13 +4,13 @@ import Project from "../Project";
 
 function Portfolio () {
     const [projects] = useState([
-        { title: 'P.A.R.K.O.U.R.', link1: 'https://parkour-review.herokuapp.com/', link2: 'https://github.com/angiebunk1/playground-rating-app', label: 'project1' },
-        { title: 'Tech Blog', link1: 'https://desolate-temple-40613.herokuapp.com/', link2: 'https://github.com/angiebunk1/Tech-Blog', label: 'project2' },
-        { title: 'Marvel Story Generator', link1: 'https://kaylab78.github.io/marvel-story-generator/', link2: 'https://github.com/angiebunk1/marvel-story-generator', label: 'project3' },
-        { title: 'Note Taker', link1: 'https://blooming-springs-91895.herokuapp.com/', link2: 'https://github.com/angiebunk1/note-taker', label: 'project4' },
-        { title: 'Work Day Scheduler', link1: 'https://angiebunk1.github.io/workday-calendar/', link2: 'https://github.com/angiebunk1/workday-calendar', label: 'project5' },
-        { title: 'Social Network API', link1: 'https://drive.google.com/file/d/1kp9_RlfqPxngRzZ9xGaJmwp4Gpg05XfW/view?usp=sharing', link2: 'https://github.com/angiebunk1/social-network-api', label: 'project6' }
-      ]);
+        { title: 'Where Were You When', link1: 'https://where-were-you-when.herokuapp.com/', link2: 'https://github.com/angiebunk1/Where-Were-You-When', label: 'project1' }
+        { title: 'P.A.R.K.O.U.R.', link1: 'https://parkour-review.herokuapp.com/', link2: 'https://github.com/angiebunk1/playground-rating-app', label: 'project2' },
+        { title: 'Tech Blog', link1: 'https://desolate-temple-40613.herokuapp.com/', link2: 'https://github.com/angiebunk1/Tech-Blog', label: 'project3' },
+        { title: 'Marvel Story Generator', link1: 'https://kaylab78.github.io/marvel-story-generator/', link2: 'https://github.com/angiebunk1/marvel-story-generator', label: 'project4' },
+        { title: 'Note Taker', link1: 'https://blooming-springs-91895.herokuapp.com/', link2: 'https://github.com/angiebunk1/note-taker', label: 'project5' },
+        { title: 'Work Day Scheduler', link1: 'https://angiebunk1.github.io/workday-calendar/', link2: 'https://github.com/angiebunk1/workday-calendar', label: 'project6' },
+        ]);
 
       return (
         <div className="portfolio">
