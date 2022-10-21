d729fcaa7434c0bf90665728f1078721a8971f97
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-10-21 16:45:32 -0500

    added details to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,12 +4,12 @@ import Project from "../Project";
 
 function Portfolio () {
     const [projects] = useState([
-        { title: 'Where Were You When', link1: 'https://where-were-you-when.herokuapp.com/', link2: 'https://github.com/angiebunk1/Where-Were-You-When', label: 'project1' },
-        { title: 'P.A.R.K.O.U.R.', link1: 'https://parkour-review.herokuapp.com/', link2: 'https://github.com/angiebunk1/playground-rating-app', label: 'project2' },
-        { title: 'Tech Blog', link1: 'https://desolate-temple-40613.herokuapp.com/', link2: 'https://github.com/angiebunk1/Tech-Blog', label: 'project3' },
-        { title: 'Marvel Story Generator', link1: 'https://kaylab78.github.io/marvel-story-generator/', link2: 'https://github.com/angiebunk1/marvel-story-generator', label: 'project4' },
-        { title: 'Note Taker', link1: 'https://blooming-springs-91895.herokuapp.com/', link2: 'https://github.com/angiebunk1/note-taker', label: 'project5' },
-        { title: 'Work Day Scheduler', link1: 'https://angiebunk1.github.io/workday-calendar/', link2: 'https://github.com/angiebunk1/workday-calendar', label: 'project6' },
+        { title: 'Where Were You When', link1: 'https://where-were-you-when.herokuapp.com/', link2: 'https://github.com/angiebunk1/Where-Were-You-When', label: 'project1', description: 'A social media app for recording memories', tech: 'Featured skill: MERN stack' },
+        { title: 'P.A.R.K.O.U.R.', link1: 'https://parkour-review.herokuapp.com/', link2: 'https://github.com/angiebunk1/playground-rating-app', label: 'project2', description: 'A playground rating app', tech: 'Featured skill: SQL'  },
+        { title: 'Tech Blog', link1: 'https://desolate-temple-40613.herokuapp.com/', link2: 'https://github.com/angiebunk1/Tech-Blog', label: 'project3', description: 'A blog site for tech enthusiasts', tech: 'Featured skill: full-stack JavaScript'  },
+        { title: 'Marvel Story Generator', link1: 'https://kaylab78.github.io/marvel-story-generator/', link2: 'https://github.com/angiebunk1/marvel-story-generator', label: 'project4', description: 'Randomized writing prompts for fanfiction authors', tech: 'Featured skill: CSS framework'  },
+        { title: 'Note Taker', link1: 'https://blooming-springs-91895.herokuapp.com/', link2: 'https://github.com/angiebunk1/note-taker', label: 'project5', description: 'An app for recording and saving notes and to-do lists', tech: 'Featured skill: Express.js'  },
+        { title: 'Work Day Scheduler', link1: 'https://angiebunk1.github.io/workday-calendar/', link2: 'https://github.com/angiebunk1/workday-calendar', label: 'project6', description: 'An app for scheduling work tasks', tech: 'Featured skill: APIs' },
         ]);
 
       return (
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -3,7 +3,7 @@ import React from "react";
 
 
 function Project ({ project }) {
-    const {title, link1, link2, label } = project
+    const {title, link1, link2, label, description, tech } = project
 
 return (
 
@@ -11,16 +11,21 @@ return (
         <div>
         <div>
         <p>
+        <h3>{title}</h3>
+        {description}
+        </p>
+        <p>
+            {tech}
+        </p>
+        <p>
         <a href={link1} target='_blank'>Deployed Site  |  </a>
         <a href={link2} target='_blank'>Github Repo</a>
         </p>
         </div>   
         <div className={label}>
-            <h2 className="project-titles">
-            {title}
-            </h2>
-
+            
         </div>
+      
         
         </div>
     </>
