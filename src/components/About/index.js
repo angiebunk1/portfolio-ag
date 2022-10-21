85d75c4d5aa44fca1cab8a09a8cc3642b350a05f
angiebunk1 <103800228+angiebunk1@users.noreply.github.com>
2022-10-21 15:10:45 -0500

    trying to fix formatting of paragraphs

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,17 +11,13 @@ function About() {
       <div className='bio'>
         <p>
         Hi! I'm Angie.
-        </p>
-        <p>
+        <br></br>
         I am a reader, a writer, a creator, a contemplator, a collector, an organizer of information.  I hail from the rocky bluffs of Wisconsin (Meskonsing), the savannah plains of Cameroon (Adamawa), the wooded mountains of Washington (Yamakiasham Yaina), and the river valleys of Minnesota (Mni Sota).  I currently live on a small homestead with my family and an assortment of pets and livestock.  
-        </p>
-        <p>
+        <br></br>
         I have a Master’s degree in Library and Information Science, and have worked in public libraries for over a decade.  I was drawn to libraries because I have a passion for connecting people with information.  It is that same passion that drives me to pursue a new career in web development.
-        </p>
-        <p>
+        <br></br>
         I recently earned a certificate in full-stack web development from the University of Minnesota.  My strengths are in back-end skills such as SQL and MongoDB.  I have developed several fully functional applications, some independently and some as part of a team.  I am known for being an ideas-generator and proactive collaborator, staying receptive to team members’ needs and adaptive to change throughout the lifecycle of product development.
-        </p>
-        <p>
+        <br></br>
         I am excited to bring my skills to a dynamic team dedicated to creating high-quality web applications.  
         </p>
       </div>
